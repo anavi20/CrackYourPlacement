3406b7ee402bb14b7ef51989efaa0aab9b64b2a1
anavi20 <105961909+anavi20@users.noreply.github.com>
2024-07-17 22:05:30 +0530

    Update maximal-square.cpp

--- Dynamic Programming/maximal-square.cpp
+++ Dynamic Programming/maximal-square.cpp
@@ -18,3 +18,28 @@ public:
         return maxArea * maxArea;
     }
 };
+
+// Time Complexity:- O(nm)  Space Complexity:- O(2n)
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i][j-1] == '1') {
+                    curr[j] = min(curr[j - 1], min(prev[j - 1], prev[j])) + 1;
+                    maxArea = max(maxArea, curr[j]);
+                }else{
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+
+        return maxArea * maxArea;
+    }
+};
